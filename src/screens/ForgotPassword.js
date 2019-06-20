782185847594c5f26606590e1d584f694f79a473
ejariasm <ejariasm@unal.edu.co>
2019-06-20 06:31:28 -0500

    Olvide contraseña

---
+++ src/screens/ForgotPassword.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import colors from '../styles/colors';
+import InputField from '../components/form/InputField';
+import Notification from '../components/Notification';
+import NextArrowButton from '../components/buttons/NextArrowButton';
+import Loader from '../components/Loader';
+
+export default class ForgotPassword extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+      formValid: true,
+      loadingVisible: false,
+      validEmail: false,
+      emailAddress: '',
+  	};
+  	this.handleEmailChange = this.handleEmailChange.bind(this);
+  	this.goToNextStep = this.goToNextStep.bind(this);
+  	this.handleCloseNotification = this.handleCloseNotification.bind(this);
+  }
+
+  handleEmailChange(email) {
+    const emailCheckRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    this.setState({ emailAddress: email });
+
+    if (!this.state.validEmail) {
+      if (emailCheckRegex.test(email)) {
+      	this.setState({ validEmail: true });
+      }
+    } else {
+      if (!emailCheckRegex.test(email)) {
+        this.setState({ validEmail: false });
+      }
+    }
+  }
+
+  goToNextStep() {
+    this.setState({ loadingVisible: true });
+    setTimeout(() => {
+      if (this.state.emailAddress === 'wrong@email.com') {
+        this.setState({
+          loadingVisible: false,
+          formValid: false,
+        });
+      } else {
+        this.setState({
+          loadingVisible: false,
+          formValid: true,
+        });
+      }
+    }, 2000);
+  }
+
+  handleCloseNotification() {
+    this.setState({ formValid: true });
+  }
+
+  render() {
+  	const { loadingVisible, formValid, validEmail } = this.state;
+    const background = formValid ? colors.green01 : colors.googleColor;
+    const showNotification = formValid ? false : true;
+    return (
+      <KeyboardAvoidingView
+        style={[{backgroundColor: background}, styles.wrapper]}
+        behavior="padding"
+      >
+        <View style={styles.scrollViewWrapper}>
+          <ScrollView style={styles.scrollView}>
+            <Text style={styles.forgotPasswordHeading}>¿Has olvidado la contraseña?</Text>
+          <Text style={styles.forgotPasswordSubheading}>Ingresa tu correo para ayudarnos encontrar tu cuenta</Text>
+            <InputField
+              customStyle={{marginBottom: 30}}
+              textColor={colors.white}
+              labelText="CORREO ELECTRONICO"
+              labelTextSize={14}
+              labelColor={colors.white}
+              borderBottomColor={colors.white}
+              inputType="email"
+              onChangeText={this.handleEmailChange}
+              showCheckmark={validEmail}
+            />
+          </ScrollView>
+
+          <View style = {styles.nextButtonWrapper}>
+          <NextArrowButton
+            handleNextButton={this.goToNextStep}
+            disabled={!validEmail}
+          />
+          </View>
+
+          <View style={styles.notificationWrapper}>
+            <Notification
+              showNotification={showNotification}
+              handleCloseNotification={this.handleCloseNotification}
+              type="Error"
+              firstLine="No existe esta cuenta para la solicitud de "
+              secondLine="correo electronico."
+            />
+          </View>
+        </View>
+        <Loader
+          modalVisible={loadingVisible}
+          animationType="fade"
+        />
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+  	display: 'flex',
+  	flex: 1,
+  },
+  scrollViewWrapper: {
+    marginTop: 70,
+    flex: 1,
+  },
+  scrollView: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 20,
+    flex: 1,
+  },
+  forgotPasswordHeading: {
+  	fontSize: 28,
+  	color: colors.white,
+  	fontWeight: '300',
+  },
+  forgotPasswordSubheading: {
+    color: colors.white,
+    fontWeight: '600',
+    fontSize: 15,
+    marginTop: 10,
+    marginBottom: 60,
+  },
+  notificationWrapper: {
+  	position: 'absolute',
+  	bottom: 0,
+  },
+
+  nextButtonWrapper:{
+    alignItems: 'flex-end',
+    right: 20,
+    bottom: 20,
+  },
+});
