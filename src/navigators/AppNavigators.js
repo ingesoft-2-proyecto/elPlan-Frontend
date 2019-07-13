0fc99bebf0aa3f5f6438ed7a11ba655fc6b4f614
Carlos Nieto Tinoco <canietot@unal.edu.co>
2019-07-13 18:34:02 -0500

    Vistas navigator

---
+++ src/navigators/AppNavigators.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addNavigationsHelpers, StackNavigator } from 'react-navigation';
+import LoggedOut from '../screens/LoggedOut';
+import Login from '../screens/Login';
+import ForgotPassword from '../screens/ForgotPassword';
+
+export const AppNavigator = StackNavigator ({
+  LoggedOut: {screen: LoggedOut},
+  Login: {screen: Login},
+  ForgotPassword: {screen: ForgotPassword},
+});
+
+const AppWithNavigationState = ({dispatch, nav, listener}) => (
+   <AppNavigator navigation = {addNavigationsHelpers ({dispatch, state: nav, addListener: listener})}/>
+ );
+
+ AppWithNavigationState.PropTypes = {
+   dispatch: PropTypes.func.isRequired,
+   nav: PropTypes.object.isRequired,
+ };
+
+ const mapStateToProps = state => ({
+   nav: state.nav,
+ });
+
+
+export default connect (mapStateToProps)(AppWithNavigationState);
